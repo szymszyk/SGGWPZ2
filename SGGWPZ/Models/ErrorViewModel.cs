a0a0a8235511c5569867700aad2dfc3f13d3076d
przemyslaw.walczyk95 <przemczan1236@gmail.com>
2017-12-03 13:44:01 +0100

    test

---
+++ SGGWPZ/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SGGWPZ.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
